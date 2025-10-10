465e42fd35a39412e511abe1a3f75e6372243659
Ker43d <53887958+Ker43d@users.noreply.github.com>
2025-10-09 20:11:49 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+const CACHE = "tlw-v1";
+const ASSETS = ["./","./index.html","./manifest.json","./app-icon-192.png","./app-icon-512.png"];
+
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+
